ea7fb420ccae31d239ce55f65216e41b5aec7aa6
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-06-27 12:54:01 +0700

    feat(settings): add read and write function

--- cbuffer.c
+++ cbuffer.c
@@ -35,16 +35,46 @@ void cb_clear(cbuffer_t *cb)
 }
 uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes)
 {
-  // if (buf == NULL || nbytes == 0)
-  // {
-  //   return 0;
-  // }
-  // if (cb == NULL || cb->active == false)
-  // {
-  //   cb_init(cb, buf, nbytes);
-  //   return nbytes;
-  // }
+  uint32_t index;
+  uint8_t *tmp;
+  if ((cb != NULL) && (buf != NULL) && (cb->overflow == FALSE))
+  {
+    for (index = 0; index < nbytes; index++)
+    {
+      if ((cb->reader) == 0)
+        if ((cb->writer) == (cb->size - 1))
+        {
+          cb->overflow = cb->overflow + 1;
+        }
+        else if ((cb->writer + 1) == cb->reader)
+        {
+          cb->overflow = cb->overflow + 1;
+        }
+      tmp = (uint8_t *)buf;
+      *(cb->data + cb->writer) = *tmp;
+      cb->writer = cb->writer + 1;
+      if ((cb->writer) == (cb->size))
+        cb->writer = 0;
+    }
+    return index;
+  }
 }
 uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes)
 {
+  uint32_t index;
+  uint8_t *tmp;
+  if ((cb != NULL) && (buf != NULL))
+  {
+    for (index = 0; index < nbytes; index++)
+    {
+      if ((cb->reader) == (cb->writer))
+        break;
+      tmp = (uint8_t *)buf;
+      *tmp = *(cb->data + cb->reader);
+      cb->reader = cb->reader + 1;
+      if ((cb->reader) == (cb->size))
+        cb->reader = 0;
+    }
+    return index;
+  }
 }
