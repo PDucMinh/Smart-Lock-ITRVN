d0107ab86092d7f0b96f77c7e95af4f839caa153
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-07-02 13:44:55 +0700

    update(write): Add macro CHECK and using memmove to write data to circular buffer

--- cbuffer.c
+++ cbuffer.c
@@ -1,5 +1,18 @@
 #include "cbuffer.h"
 
+#define CHECK_NULL(A, B) \
+  do                     \
+  {                      \
+    if (!A)              \
+      return B;          \
+  } while (0);
+#define CHECK_MAX(A, MAX) \
+  do                      \
+  {                       \
+    if (A > MAX)          \
+      return;             \
+  } while (0);
+
 void cb_init(cbuffer_t *cb, void *buf, uint32_t size)
 {
   if (size == 0)
@@ -35,28 +48,67 @@ void cb_clear(cbuffer_t *cb)
 }
 uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes)
 {
-  uint32_t index;
-  uint8_t *tmp;
-  if ((cb != NULL) && (buf != NULL) && (cb->overflow == FALSE))
+  CHECK_NULL(cb, NULL);
+  CHECK_NULL(buf, NULL);
+  uint32_t space_cnt = cb_space_count(cb);
+  CHECK_NULL(space_cnt, 0);
+  if ((cb->writer) >= (cb->reader))
   {
-    for (index = 0; index < nbytes; index++)
+    int nbytes_tail = 0;
+    uint8_t *dst_tail, *src_tail;
+    dst_tail = (cb->data) + (cb->writer);
+    src_tail = buf;
+    if (nbytes <= (cb->size - cb->writer))
     {
-      if ((cb->reader) == 0)
-        if ((cb->writer) == (cb->size - 1))
-        {
-          cb->overflow = cb->overflow + 1;
-        }
-        else if ((cb->writer + 1) == cb->reader)
-        {
-          cb->overflow = cb->overflow + 1;
-        }
-      tmp = (uint8_t *)buf;
-      *(cb->data + cb->writer) = *tmp;
-      cb->writer = cb->writer + 1;
-      if ((cb->writer) == (cb->size))
-        cb->writer = 0;
+      nbytes_tail = nbytes;
+      memmove((void *)dst_tail, (void *)src_tail, nbytes_tail);
+      cb->writer += nbytes;
+      cb->overflow = 0;
+      return nbytes;
     }
-    return index;
+    else if (nbytes <= space_cnt)
+    {
+      nbytes_tail = cb->size - cb->writer;
+      int nbytes_head = nbytes - nbytes_tail;
+      uint8_t *dst_head, *src_head;
+      dst_head = cb->data;
+      src_head = (uint8_t *)buf + nbytes_tail;
+      memmove((void *)dst_tail, (void *)src_tail, nbytes_tail);
+      memmove((void *)dst_head, (void *)src_head, nbytes_head);
+      cb->writer = nbytes_head;
+      cb->overflow = 0;
+      return nbytes;
+    }
+    else
+    {
+      nbytes_tail = cb->size - cb->writer;
+      int nbytes_head = space_cnt - nbytes_tail;
+      uint8_t *dst_head, *src_head;
+      dst_head = cb->data;
+      src_head = (uint8_t *)buf + nbytes_tail;
+      memmove((void *)dst_tail, (void *)src_tail, nbytes_tail);
+      memmove((void *)dst_head, (void *)src_head, nbytes_head);
+      cb->writer = nbytes_head;
+      cb->overflow = nbytes - space_cnt;
+      return space_cnt;
+    }
+  }
+  else
+  {
+    if (nbytes <= space_cnt)
+    {
+      memmove((void *)(cb->data + cb->writer), buf, nbytes);
+      cb->writer += nbytes;
+      cb->overflow = 0;
+      return nbytes;
+    }
+    else
+    {
+      memmove((void *)(cb->data + cb->writer), buf, space_cnt);
+      cb->writer += space_cnt;
+      cb->overflow = nbytes - space_cnt;
+      return space_cnt;
+    }
   }
 }
 uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes)
