7a518d4db0b9c592eb0e73583e1d6e6e24b883d4
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-07-02 12:37:17 +0700

    update(read): Add macro CHECK and using memmove to read data from circular buffer

--- cbuffer.c
+++ cbuffer.c
@@ -1,5 +1,18 @@
 #include "cbuffer.h"
 
+#define CHECK_NULL(A, B) \
+  do                     \
+  {                      \
+    if (!A)              \
+      return B;          \
+  } while (0);
+#define CHECK_MAX(A, MAX) \
+  do                      \
+  {                       \
+    if (A > MAX)          \
+      return;             \
+  } while (0);
+
 void cb_init(cbuffer_t *cb, void *buf, uint32_t size)
 {
   if (size == 0)
@@ -61,20 +74,61 @@ uint32_t cb_write(cbuffer_t *cb, void *buf, uint32_t nbytes)
 }
 uint32_t cb_read(cbuffer_t *cb, void *buf, uint32_t nbytes)
 {
-  uint32_t index;
-  uint8_t *tmp;
-  if ((cb != NULL) && (buf != NULL))
+  CHECK_NULL(cb, NULL);
+  CHECK_NULL(buf, NULL);
+  uint32_t data_cnt = cb_data_count(cb);
+  CHECK_NULL(data_cnt, 0);
+  if ((cb->writer) > (cb->reader))
   {
-    for (index = 0; index < nbytes; index++)
+    if (nbytes < data_cnt)
     {
-      if ((cb->reader) == (cb->writer))
-        break;
-      tmp = (uint8_t *)buf;
-      *tmp = *(cb->data + cb->reader);
-      cb->reader = cb->reader + 1;
-      if ((cb->reader) == (cb->size))
-        cb->reader = 0;
+      memmove(buf, (void *)(cb->data + cb->reader), nbytes);
+      cb->reader += nbytes;
+      return nbytes;
     }
-    return index;
+    else
+    {
+      memmove(buf, (void *)(cb->data + cb->reader), data_cnt);
+      cb->reader += data_cnt;
+      return data_cnt;
+    }
+  }
+  else
+  {
+    int nbytes_tail = 0;
+    uint8_t *dst_tail, *src_tail;
+    src_tail = (cb->data) + (cb->reader);
+    dst_tail = buf;
+    if (nbytes < (cb->size - cb->reader))
+    {
+      nbytes_tail = nbytes;
+      memmove((void *)dst_tail, (void *)src_tail, nbytes_tail);
+      cb->reader += nbytes;
+      return nbytes;
+    }
+    else if (nbytes <= data_cnt)
+    {
+      nbytes_tail = cb->size - cb->reader;
+      int nbytes_head = nbytes - nbytes_tail;
+      uint8_t *dst_head, *src_head;
+      src_head = cb->data;
+      dst_head = (uint8_t *)buf + nbytes_tail;
+      memmove((void *)dst_tail, (void *)src_tail, nbytes_tail);
+      memmove((void *)dst_head, (void *)src_head, nbytes_head);
+      cb->reader = nbytes_head;
+      return nbytes;
+    }
+    else
+    {
+      nbytes_tail = cb->size - cb->writer;
+      int nbytes_head = data_cnt - nbytes_tail;
+      uint8_t *dst_head, *src_head;
+      src_head = cb->data;
+      dst_head = (uint8_t *)buf + nbytes_tail;
+      memmove((void *)dst_tail, (void *)src_tail, nbytes_tail);
+      memmove((void *)dst_head, (void *)src_head, nbytes_head);
+      cb->reader = nbytes_head;
+      return data_cnt;
+    }
   }
 }
