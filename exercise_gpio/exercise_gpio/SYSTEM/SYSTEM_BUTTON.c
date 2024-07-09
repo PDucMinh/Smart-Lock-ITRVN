be6185ae2c47a888ff4eb3d6fc981b0aa66d04bc
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-07-09 09:54:17 +0700

    feat: button detection

--- exercise_gpio/exercise_gpio/SYSTEM/SYSTEM_BUTTON.c
+++ exercise_gpio/exercise_gpio/SYSTEM/SYSTEM_BUTTON.c
@@ -5,10 +5,10 @@
  * @version    major.minor.patch
  * @date       2024-07-07
  * @author     Minh Pham Duc
- *             
+ *
  * @brief      <A brief description of the content of the file>
- *             
- * @note          
+ *
+ * @note
  * @example    example_file_1.c
  *             Example_1 description
  * @example    example_file_2.c
@@ -16,35 +16,19 @@
  */
 
 /* Includes ----------------------------------------------------------- */
-#include "code_template.h"
+#include "SYSTEM_BUTTON.h"
+#include "DRIVER_BUTTON.h"
 
 /* Private defines ---------------------------------------------------- */
-#define PRIVATE_DEFINE_1  (0) /*!< Description of PRIVATE_DEFINE_1 */
-#define PRIVATE_DEFINE_2  (0) /*!< Description of PRIVATE_DEFINE_2 */
-
+#define PRIVATE_DEFINE_1 (0) /*!< Description of PRIVATE_DEFINE_1 */
+#define PRIVATE_DEFINE_2 (0) /*!< Description of PRIVATE_DEFINE_2 */
 /* Private enumerate/structure ---------------------------------------- */
-/**
- * @brief <enum descriptiton>
- */
-typedef enum 
+typedef enum
 {
-  PRIVATE_ENUM_1, /**< Description of PRIVATE_ENUM_1 */
-  PRIVATE_ENUM_2, /**< Description of PRIVATE_ENUM_2 */
-  PRIVATE_ENUM_3  /**< Description of PRIVATE_ENUM_3 */
-}
-private_enum_type_t;
-
-/**
- * @brief <structure descriptiton>
- */
-typedef struct 
-{
-  uint32_t member_1, /**< Description of member_1 */
-  uint32_t member_2, /**< Description of member_2 */
-  uint32_t member_3  /**< Description of member_3 */
-}
-private_struct_type_t;
-
+  STATE_CLICK,
+  STATE_OFF,
+  STATE_INIT
+} button_state_process_t;
 /* Private macros ----------------------------------------------------- */
 /**
  * @brief  <macro description>
@@ -55,44 +39,125 @@ private_struct_type_t;
  *
  * @attention  <API attention note>
  *
- * @return  
+ * @return
  *  - 0: Success
  *  - 1: Error
  */
-#define PRIVATE_MACRO(a)  do_something_with(a)
+#define PRIVATE_MACRO(a) do_something_with(a)
 
 /* Public variables --------------------------------------------------- */
-int g_var_1;
-int g_var_2;
 
 /* Private variables -------------------------------------------------- */
-static int var_1; /**< Description of private variable var_1 */
-static int var_2; /**< Description of private variable var_2 */
-
+int system_button_timer;
+char system_button_timeout;
+int click;
+button_state_process_t button_process;
 /* Private function prototypes ---------------------------------------- */
-/**
- * @brief  <function description>
- *
- * @param[in]     <param_name>  <param_despcription>
- * @param[out]    <param_name>  <param_despcription>
- * @param[inout]  <param_name>  <param_despcription>
- *
- * @attention  <API attention note>
- *
- * @return  
- *  - 0: Success
- *  - 1: Error
- */
-static void private_function(void);
+void timer_run();
+char time_out();
+void set_timer(int time);
+void system_button_set_state(driver_button_t* db, driver_state_t *errorCode);
 /* Function definitions ----------------------------------------------- */
-system_button_state_t system_button_read(driver_button_t* db){
-  
-}
-void system_button_loop(driver_button_t* db){
-
-}
-/* Private definitions ----------------------------------------------- */
-static void private_function(void)
+void system_button_init(driver_button_t* db)
 {
+  click = 0;
+  system_button_timeout = 0;
+  button_process = STATE_INIT;
+  system_button_timer = 0;
+}
+
+system_button_state_t system_button_read(driver_button_t* db)
+{
+  if (click == 1)
+  {
+    return SYSTEM_BUTTON_1CLICK;
+  }
+  if (click == 2)
+  {
+    return SYSTEM_BUTTON_2CLICK;
+  }
+  if (click > 2)
+  {
+    return SYSTEM_BUTTON_HOLD;
+  }
+  return SYSTEM_BUTTON_RELEASE;
+}
+
+void system_button_loop(driver_button_t *db, driver_state_t *errorCode)
+{
+  timer_run();
+  system_button_set_state(db, errorCode);
+}
+
+/* Private definitions ----------------------------------------------- */
+void timer_run()
+{
+  if (system_button_timer > 0)
+  {
+    system_button_timer--;
+    if (system_button_timer <= 0)
+    {
+      system_button_timeout = 1;
+    }
+  }
+}
+
+void system_button_set_state(driver_button_t* db, driver_state_t *errorCode)
+{
+  switch (button_process)
+  {
+  case STATE_CLICK:
+    if ((driver_button_read(db, errorCode) == DRIVER_BUTTON_IS_PUSHED) && (time_out()))
+    {
+      click++;
+      set_timer(500);
+    }
+    if (driver_button_read(db, errorCode) == DRIVER_BUTTON_NO_PUSHED)
+    {
+      set_timer(300);
+      button_process = STATE_OFF;
+    }
+    break;
+  case STATE_OFF:
+    if ((driver_button_read(db, errorCode) == DRIVER_BUTTON_NO_PUSHED) && (time_out()))
+    {
+      click = 0;
+      button_process = STATE_INIT;
+    }
+    if (driver_button_read(db, errorCode) == DRIVER_BUTTON_IS_PUSHED)
+    {
+      click++;
+      set_timer(500);
+      button_process = STATE_CLICK;
+    }
+    break;
+  case STATE_INIT:
+    if (driver_button_read(db, errorCode) == DRIVER_BUTTON_IS_PUSHED)
+    {
+      click++;
+      set_timer(500);
+      button_process = STATE_CLICK;
+    }
+    break;
+  default:
+    button_process = STATE_INIT;
+    break;
+  }
+}
+
+char time_out()
+{
+  if (system_button_timeout)
+  {
+    system_button_timeout = 0;
+    return 1;
+  }
+  return 0;
+}
+
+void set_timer(int time)
+{
+  system_button_timer = time;
+  system_button_timeout = 0;
 }
 /* End of file -------------------------------------------------------- */
--- exercise_gpio/exercise_gpio/SYSTEM/SYSTEM_BUTTON.h
+++ exercise_gpio/exercise_gpio/SYSTEM/SYSTEM_BUTTON.h
@@ -30,12 +30,23 @@
  */
 typedef enum
 {
+  SYSTEM_BUTTON_IDLE,
+  SYSTEM_BUTTON_INIT,
   SYSTEM_BUTTON_1CLICK, /**< Description of PUBLIC_ENUM_1 */
   SYSTEM_BUTTON_2CLICK, /**< Description of PUBLIC_ENUM_2 */
   SYSTEM_BUTTON_HOLD,
   SYSTEM_BUTTON_RELEASE /**< Description of PUBLIC_ENUM_3 */
 } system_button_state_t;
 
+typedef struct 
+{
+  system_button_state_t button_state;
+  driver_button_t* button;
+  driver_state_t activate;
+}system_button_t;
+/**
+ * @brief structure manage a button
+ */
 /* Public macros ------------------------------------------------------ */
 /**
  * @brief  <macro description>
@@ -50,7 +61,6 @@ typedef enum
  *  - 0: Success
  *  - 1: Error
  */
-#define PUBLIC_MACRO(a) do_something_with(a)
 
 /* Public variables --------------------------------------------------- */
 extern int g_var_1; /**< Description of public variable g_var_1 */
@@ -70,8 +80,42 @@ extern int g_var_2; /**< Description of public variable g_var_2 */
  *  - 0: Success
  *  - 1: Error
  */
+void system_button_init(driver_button_t* db);
+/**
+ * @brief  <initialize counter before reading number of clicks>
+ *
+ * @param[in]     driver_button_t struct pointer manage button signal
+ * @param[out]    none 
+ *
+ * @attention  none
+ *
+ * @return
+ *  none
+ */
 system_button_state_t system_button_read(driver_button_t* db);
-void system_button_loop(driver_button_t* db);
+/**
+ * @brief  <read button state>
+ *
+ * @param[in]     driver_button_t struct pointer manage button signal
+ * @param[out]    system_button_state_t  button behavior number of click or release
+ *
+ * @attention  none
+ *
+ * @return
+ *  system_button_state_t: number of the button clicks
+ */
+void system_button_loop(driver_button_t* db, driver_state_t *errorCode);
+/**
+ * @brief  <button click counter run() in TIMER>
+ *
+ * @param[in]     driver_button_t struct pointer manage button signal
+ * @param[out]    none
+ *
+ * @attention  none
+ *
+ * @return
+ *  none
+ */
 #endif // __CODE_TEMPLATE_H
 
 /* End of file -------------------------------------------------------- */
