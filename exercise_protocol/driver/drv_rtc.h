60ff4645abc5d0e792919c084f9825371e6c2f35
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-07-23 11:30:28 +0700

    update: update struct data for time read from rtc, modify reg data type to defined constant , add defined constant for day of week value

--- exercise_protocol/driver/drv_rtc.h
+++ exercise_protocol/driver/drv_rtc.h
@@ -23,56 +23,55 @@
 /* Includes ----------------------------------------------------------- */
 #include "drv_mcu.h"
 /* Public defines ----------------------------------------------------- */
-#define PUBLIC_DEFINE_1  (0) /*!< Description of PUBLIC_DEFINE_1 */
-#define PUBLIC_DEFINE_2  (0) /*!< Description of PUBLIC_DEFINE_2 */
+/* rtc register ID */
+#define DRV_RTC_REG_SECOND  (0x00)
+#define DRV_RTC_REG_MINUTE  (0x01)
+#define DRV_RTC_REG_HOUR    (0x02)
+#define DRV_RTC_REG_DAY     (0x03)
+#define DRV_RTC_REG_DATE    (0x04)
+#define DRV_RTC_REG_MONTH   (0x05)
+#define DRV_RTC_REG_YEAR    (0x06)
+#define DRV_RTC_REG_CONTROL (0x07)
+
+/* rtc day ID */
+#define DRV_RTC_DAY_SUN  (0x01)
+#define DRV_RTC_DAY_MON  (0x02)
+#define DRV_RTC_DAY_TUE  (0x03)
+#define DRV_RTC_DAY_WES  (0x04)
+#define DRV_RTC_DAY_THU  (0x05)
+#define DRV_RTC_DAY_FRI  (0x06)
+#define DRV_RTC_DAY_SAT  (0x07)
 
 /* Public enumerate/structure ----------------------------------------- */
-/**
- * @brief <enum descriptiton>
- */
-typedef enum 
-{
-  DRV_RTC_REG_SECOND = 0x00, /**< Description of PUBLIC_ENUM_1 */
-  DRV_RTC_REG_MINUTE = 0x01, /**< Description of PUBLIC_ENUM_2 */
-  DRV_RTC_REG_HOUR = 0x02,
-  DRV_RTC_REG_DAY = 0x03,
-  DRV_RTC_REG_DATE = 0x04,
-  DRV_RTC_REG_MONTH = 0x05,
-  DRV_RTC_REG_YEAR = 0x06,
-  DRV_RTC_REG_CONTROL = 0x07    /**< Description of PUBLIC_ENUM_3 */
-}
-drv_rtc_reg_t;
 
 /**
  * @brief <structure descriptiton>
  */
 typedef struct 
 {
-  drv_rtc_reg_t reg; /**< Description of member_1 */
   drv_mcu_pin_t data_pin; /**< Description of member_2 */
   drv_mcu_pin_t clk_pin;  /**< Description of member_3 */
 }
 drv_rtc_t;
 
-/* Public macros ------------------------------------------------------ */
 /**
- * @brief  <macro description>
- *
- * @param[in]     <param_name>  <param_despcription>
- * @param[out]    <param_name>  <param_despcription>
- * @param[inout]  <param_name>  <param_despcription>
- *
- * @attention  <API attention note>
- *
- * @return  
- *  - 0: Success
- *  - 1: Error
+ * @brief <structure descriptiton>
  */
-#define PUBLIC_MACRO(a)  do_something_with(a)
+typedef struct 
+{
+  uint8_t second; /**< Description of member_1 */
+  uint8_t minute; /**< Description of member_2 */
+  uint8_t hour;
+  uint8_t day;
+  uint8_t date;
+  uint8_t month;
+  uint8_t year;  /**< Description of member_3 */
+}
+drv_rtc_time_t;
+
+/* Public macros ------------------------------------------------------ */
 
 /* Public variables --------------------------------------------------- */
-extern int g_var_1; /**< Description of public variable g_var_1 */
-extern int g_var_2; /**< Description of public variable g_var_2 */
 
 /* Public function prototypes ----------------------------------------- */
 /**
@@ -90,10 +89,10 @@ extern int g_var_2; /**< Description of public variable g_var_2 */
  */
  drv_state_t drv_rtc_init(drv_mcu_t* v_dmcu, drv_rtc_t* v_drtc);
  drv_state_t drv_rtc_enable_clk(drv_mcu_t* v_dmcu, drv_rtc_t* v_drtc);
- drv_state_t drv_rtc_read(drv_mcu_t* v_dmcu, drv_rtc_t* v_drtc);
- drv_state_t drv_rtc_write(drv_mcu_t* v_dmcu, drv_rtc_t* v_drtc);
+ drv_rtc_time_t drv_rtc_read(drv_mcu_t* v_dmcu);
+ drv_state_t drv_rtc_write(drv_mcu_t* v_dmcu, drv_rtc_time_t v_dtime);
  
 
-#endif // __CODE_TEMPLATE_H
+#endif // __DRV_RTC_H
 
 /* End of file -------------------------------------------------------- */
