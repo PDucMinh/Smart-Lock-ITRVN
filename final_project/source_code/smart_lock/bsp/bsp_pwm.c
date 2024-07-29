2a0c7e59ff6c229066c3241087026c10acd21281
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-07-29 21:23:41 +0700

    update:update bsp pwm API follow bsp config

--- final_project/source_code/smart_lock/bsp/bsp_pwm.c
+++ final_project/source_code/smart_lock/bsp/bsp_pwm.c
@@ -24,45 +24,70 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-
+TIM_HandleTypeDef htim3;
+TIM_HandleTypeDef htim4;
 /* Private function prototypes ---------------------------------------- */
 
 /* Function definitions ----------------------------------------------- */
-bsp_state_t bsp_pwm_start(TIM_HandleTypeDef *htim, uint32_t channel)
+bsp_state_t bsp_pwm_start(bsp_config_id_t id, uint32_t channel)
 {
-    if (HAL_TIM_PWM_Start(htim, channel) != HAL_OK)
+  if (id == BSP_CONFIG_ID_LED_RGB)
+  {
+    if (HAL_TIM_PWM_Start(&htim3, channel) != HAL_OK)
     {
-        return BSP_STATE_FAIL;
+      return BSP_STATE_FAIL;
     }
-    else 
+    else
     {
-        return BSP_STATE_PASS;
-    }  
+      return BSP_STATE_PASS;
+    }
+  }
+  else if (id == BSP_CONFIG_ID_BUZZER)
+  {
+    if (HAL_TIM_PWM_Start(&htim4, channel) != HAL_OK)
+    {
+      return BSP_STATE_FAIL;
+    }
+    else
+    {
+      return BSP_STATE_PASS;
+    }
+  }
 }
 
-bsp_state_t bsp_pwm_stop(TIM_HandleTypeDef *htim, uint32_t channel)
+bsp_state_t bsp_pwm_stop(bsp_config_id_t id, uint32_t channel)
 {
+  if (id == BSP_CONFIG_ID_LED_RGB)
+  {
     if (HAL_TIM_PWM_Stop(htim, channel) != HAL_OK)
     {
-        return BSP_STATE_FAIL;
+      return BSP_STATE_FAIL;
     }
-    else 
+    else
     {
-        return BSP_STATE_PASS;
+      return BSP_STATE_PASS;
     }
+  }
+  else if (id == BSP_CONFIG_ID_BUZZER)
+  {
+    if (HAL_TIM_PWM_Stop(htim, channel) != HAL_OK)
+    {
+      return BSP_STATE_FAIL;
+    }
+    else
+    {
+      return BSP_STATE_PASS;
+    }
+  }
 }
 
-bsp_state_t bsp_pwm_set_duty(TIM_HandleTypeDef *htim, uint32_t channel, uint32_t duty)
+bsp_state_t bsp_pwm_set_duty(bsp_config_id_t id, uint32_t channel, uint32_t duty)
 {
-    int pulse = (((htim->Init.Period + 1) * duty) / 100) - 1;
-    if (__HAL_TIM_SET_COMPARE(htim, channel, pulse) != HAL_OK)
-    {
-       return BSP_STATE_FAIL;
-    }
-    else 
-    {
-        return BSP_STATE_PASS;
-    }
+  BSP_CHECK_RANGE(id, BSP_CONFIG_ID_MAX, BSP_STATE_FAIL);
+  BSP_CHECK_RANGE(duty, 100, BSP_STATE_FAIL);
+  int pulse = (((htim->Init.Period + 1) * duty) / 100) - 1;
+  __HAL_TIM_SET_COMPARE(htim, channel, pulse);
+  return BSP_STATE_PASS;
 }
 
 
