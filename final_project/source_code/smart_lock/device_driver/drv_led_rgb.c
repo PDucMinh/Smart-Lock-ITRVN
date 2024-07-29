c3c7da717555153fcace26ba57fa8c24d834215f
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-07-29 11:05:02 +0700

    style: add template drv_led.c

---
+++ final_project/source_code/smart_lock/device_driver/drv_led_rgb.c
@@ -0,0 +1,79 @@
+/**
+ * @file       drv_led_rgb.c
+ * @copyright  Copyright (C) 2024. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    major.minor.patch
+ * @date       yyyy-mm-dd
+ * @author     Minh Pham Duc
+ *             
+ * @brief      <A brief description of the content of the file>
+ *             
+ * @note          
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "drv_led_rgb.h"
+
+/* Private defines ---------------------------------------------------- */
+
+
+/* Private enumerate/structure ---------------------------------------- */
+/**
+ * @brief <enum descriptiton>
+ */
+
+
+/**
+ * @brief <structure descriptiton>
+ */
+
+
+/* Private macros ----------------------------------------------------- */
+/**
+ * @brief  <macro description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+
+
+/* Public variables --------------------------------------------------- */
+
+
+/* Private variables -------------------------------------------------- */
+
+
+/* Private function prototypes ---------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+
+
+/* Function definitions ----------------------------------------------- */
+
+
+/* Private definitions ----------------------------------------------- */
+
+
+/* End of file -------------------------------------------------------- */
