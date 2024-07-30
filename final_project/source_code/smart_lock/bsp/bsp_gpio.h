d33ee6d74ca4c7affc38aa0e9ee18ae8a2f25432
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-07-30 14:53:34 +0700

    feat: add API prototype for gpio read pin

--- final_project/source_code/smart_lock/bsp/bsp_gpio.h
+++ final_project/source_code/smart_lock/bsp/bsp_gpio.h
@@ -1,15 +1,15 @@
 /**
  * @file       bsp_gpio.h
  * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
- * @license    
+ * @license
  * @version    1.0.0
  * @date       2024-07-27
  * @author     Hung Nguyen
- *             
+ *
  * @brief      This file export gpio init API
- *             
- * @note          
- * @example    
+ *
+ * @note
+ * @example
  */
 
 /* Define to prevent recursive inclusion ------------------------------ */
@@ -44,43 +44,52 @@
 #define BSP_GPIO_PIN_14 (0x0E)
 #define BSP_GPIO_PIN_15 (0x0F)
 /* gpio pin mode ID */
-#define BSP_GPIO_INPUT     (GPIO_MODE_INPUT)
+#define BSP_GPIO_INPUT (GPIO_MODE_INPUT)
 #define BSP_GPIO_OUTPUT_PP (GPIO_MODE_OUTPUT_PP)
 #define BSP_GPIO_OUTPUT_OD (GPIO_MODE_OUTPUT_OD)
-#define BSP_GPIO_AF_PP     (GPIO_MODE_AF_PP)
-#define BSP_GPIO_AF_OD     (GPIO_MODE_AF_OD)
-#define BSP_GPIO_ANALOG    (GPIO_MODE_ANALOG)
+#define BSP_GPIO_AF_PP (GPIO_MODE_AF_PP)
+#define BSP_GPIO_AF_OD (GPIO_MODE_AF_OD)
+#define BSP_GPIO_ANALOG (GPIO_MODE_ANALOG)
 /* gpio pin speed ID */
 #define BSP_GPIO_FREQ_VERY_HIGH (GPIO_SPEED_FREQ_VERY_HIGH)
-#define BSP_GPIO_FREQ_HIGH      (GPIO_SPEED_FREQ_HIGH)
-#define BSP_GPIO_FREQ_MEDIUM    (GPIO_SPEED_FREQ_MEDIUM)
-#define BSP_GPIO_FREQ_LOW       (GPIO_SPEED_FREQ_LOW)
+#define BSP_GPIO_FREQ_HIGH (GPIO_SPEED_FREQ_HIGH)
+#define BSP_GPIO_FREQ_MEDIUM (GPIO_SPEED_FREQ_MEDIUM)
+#define BSP_GPIO_FREQ_LOW (GPIO_SPEED_FREQ_LOW)
 /* gpio pin resistor type */
-#define BSP_GPIO_PULLUP    (GPIO_PULLUP)
-#define BSP_GPIO_PULLDOWN  (GPIO_PULLDOWN)
-#define BSP_GPIO_NOPULL    (GPIO_NOPULL)
+#define BSP_GPIO_PULLUP (GPIO_PULLUP)
+#define BSP_GPIO_PULLDOWN (GPIO_PULLDOWN)
+#define BSP_GPIO_NOPULL (GPIO_NOPULL)
 /* gpio pin alternate function ID */
-#define BSP_GPIO_AF0    (0x00)
-#define BSP_GPIO_AF1    (0x01) 
-#define BSP_GPIO_AF2    (0x02)
-#define BSP_GPIO_AF3    (0x03)
-#define BSP_GPIO_AF4    (0x04)
-#define BSP_GPIO_AF5    (0x05)
-#define BSP_GPIO_AF6    (0x06)
-#define BSP_GPIO_AF7    (0x07)
-#define BSP_GPIO_AF8    (0x08)
-#define BSP_GPIO_AF9    (0x09)
-#define BSP_GPIO_AF10   (0x0A)
-#define BSP_GPIO_AF12   (0x0C)
-#define BSP_GPIO_AF15   (0x0F)
+#define BSP_GPIO_AF0 (0x00)
+#define BSP_GPIO_AF1 (0x01)
+#define BSP_GPIO_AF2 (0x02)
+#define BSP_GPIO_AF3 (0x03)
+#define BSP_GPIO_AF4 (0x04)
+#define BSP_GPIO_AF5 (0x05)
+#define BSP_GPIO_AF6 (0x06)
+#define BSP_GPIO_AF7 (0x07)
+#define BSP_GPIO_AF8 (0x08)
+#define BSP_GPIO_AF9 (0x09)
+#define BSP_GPIO_AF10 (0x0A)
+#define BSP_GPIO_AF12 (0x0C)
+#define BSP_GPIO_AF15 (0x0F)
 
 /* Public enumerate/structure ----------------------------------------- */
+/**
+ * @brief enum definition for gpio pin state
+ */
+typedef enum
+{
+  BSP_GPIO_SET = GPIO_PIN_SET,
+  BSP_GPIO_RESET = GPIO_PIN_RESET
+} bsp_gpio_pin_state_t;
+
 /**
  * @brief struct manage gpio pin
  */
 typedef struct
 {
-  uint16_t io; 
+  uint16_t io;
   uint16_t af;
   uint16_t mode;
   uint16_t pull_type;
@@ -100,10 +109,21 @@ typedef struct
  * @attention  This API will be called one time only
  *
  * @return     none
- *  
+ *
  */
- bsp_state_t bsp_gpio_pin_init(bsp_gpio_pin_t* bgpio);
+bsp_state_t bsp_gpio_pin_init(bsp_gpio_pin_t *bgpio);
 
+/**
+ * @brief  API for reading status of gpio pin
+ *
+ * @param[in]     pin  struct manage gpio pin
+ *
+ * @attention  This API will be called one time only
+ *
+ * @return     none
+ *
+ */
+bsp_gpio_pin_state_t bsp_gpio_pin_read(bsp_gpio_pin_t *bgpio);
 #endif // __BSP_GPIO_H
 
 /* End of file -------------------------------------------------------- */
