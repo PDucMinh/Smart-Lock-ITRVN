33a2f6edbf5997c2763f11b21fbdd5ed40d09695
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-07-31 14:44:19 +0700

    feat(fifo): init() and empty()

--- final_project/source_code/smart_lock/middle_ware/fifo_buffer.c
+++ final_project/source_code/smart_lock/middle_ware/fifo_buffer.c
@@ -24,7 +24,27 @@
 /* Private enumerate/structure ---------------------------------------- */
 
 /* Private macros ----------------------------------------------------- */
-
+#define CHECK_SIZE(A) \
+  do                 \
+  {                  \
+    if (A > FIFO_MAX_SIZE) \
+    {                \
+      return FIFO_FAIL; \
+    }                \
+    else if (A == 0) \
+    {                \
+      return FIFO_FAIL; \
+    }                \
+  } while (0)
+  
+#define CHECK_NULL(A) \
+  do                 \
+  {                  \
+    if (A <= 0)   \
+    {                \
+      return FIFO_FAIL; \
+    }                \
+  } while (0)
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
@@ -43,16 +63,38 @@
  *  - 0: Success
  *  - 1: Error
  */
+fifo_return_t fifo_set_front(fifo_buffer_info_t *fifo_buffer, uint8_t front);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+fifo_return_t fifo_set_rear(fifo_buffer_info_t *fifo_buffer, uint8_t rear);
 
 /* Function definitions ----------------------------------------------- */
 void fifo_init(fifo_buffer_info_t *fifo_buffer, uint32_t fifo_size)
 {
-
+  CHECK_SIZE(fifo_size);
+  fifo_buffer->fifo_size = fifo_size;
+  fifo_set_front(fifo_buffer, 0);
+  fifo_set_rear(fifo_buffer, 0);
 }
 
 void fifo_empty(fifo_buffer_info_t *fifo_buffer)
 {
-
+  CHECK_NULL(fifo_buffer->rear - fifo_buffer->front);
+  fifo_set_front(fifo_buffer, 0);
+  fifo_set_rear(fifo_buffer, 0);
+  //something
 }
 
 uint8_t fifo_push(fifo_buffer_info_t *fifo_buffer, void *source_data)
@@ -70,5 +112,21 @@ uint8_t fifo_size(fifo_buffer_info_t *fifo_buffer)
 
 }
 /* Private definitions ----------------------------------------------- */
+fifo_return_t fifo_set_front(fifo_buffer_info_t *fifo_buffer, uint8_t front)
+{
+  for (uint16_t i = 0; i < fifo_buffer->fifo_size; i++)
+  {
+    fifo_buffer->front = front;
+  }
+  return FIFO_OK;
+}
 
+fifo_return_t fifo_set_rear(fifo_buffer_info_t *fifo_buffer, uint8_t rear)
+{
+  for (uint16_t i = 0; i < fifo_buffer->fifo_size; i++)
+  {
+    fifo_buffer->rear = rear;
+  }
+  return FIFO_OK;
+} 
 /* End of file -------------------------------------------------------- */
