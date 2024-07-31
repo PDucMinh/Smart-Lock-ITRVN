6f5a94af0601e9d13d4b76313364bfd713941e3a
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-01 06:03:02 +0700

    update: update function pointer to drv_buzzer init API

--- final_project/source_code/smart_lock/device_driver/drv_buzzer.c
+++ final_project/source_code/smart_lock/device_driver/drv_buzzer.c
@@ -16,8 +16,7 @@
 #include "drv_buzzer.h"
 
 /* Private defines ---------------------------------------------------- */
-#define BUZZER_CHANNEL      TIM_CHANNEL_1
-#define DELAY_BEFORE_START  1
+
 /* Private enumerate/structure ---------------------------------------- */
 
 /* Private macros ----------------------------------------------------- */
@@ -28,63 +27,67 @@
 static uint8_t duration_flag = 0;
 
 /* Private function prototypes ---------------------------------------- */
-
+void drv_buzzer_duration_callback(void);
 /* Function definitions ----------------------------------------------- */
-drv_buzzer_status_t drv_buzzer_init(drv_buzzer_t *buzzer)
+drv_buzzer_status_t drv_buzzer_init(drv_buzzer_t* buzzer)
 {
-    if (buzzer == NULL)
-    {
-        return DRV_BUZZER_STATUS_FAIL;
-    }
-    
-    buzzer->pwm_stop = bsp_pwm_start(BSP_CONFIG_ID_BUZZER, BUZZER_CHANNEL); 
-    buzzer->pwm_set_feq = bbbbbb(BSP_CONFIG_ID_BUZZER, BUZZER_CHANNEL, 0); 
-    buzzer->timer_set = aaaaa((void *)drv_buzzer_duration_cplt, DELAY_BEFORE_START, 0);
-    duration_flag = 0;
+  if (buzzer == NULL)
+  {
+    return DRV_BUZZER_STATUS_FAIL;
+  }
+  buzzer->pwm_start = bsp_pwm_start;
+  buzzer->pwm_stop = bsp_pwm_stop;
+  buzzer->pwm_set_feq = bsp_pwm_set_freq;
+  duration_flag = 0;
 
-    return DRV_BUZZER_STATUS_SUCCESS;
+  return DRV_BUZZER_STATUS_SUCCESS;
 }
 
-drv_buzzer_status_t drv_buzzer_set_note(drv_buzzer_t *buzzer, uint32_t note, uint32_t duration)
+drv_buzzer_status_t drv_buzzer_set_note(drv_buzzer_t* buzzer, uint16_t note, uint32_t duration)
 {
-    if (buzzer == NULL) 
-    {
-        return DRV_BUZZER_STATUS_FAIL;
-    }
+  if (buzzer == NULL)
+  {
+    return DRV_BUZZER_STATUS_FAIL;
+  }
 
-    // Reset duration_flag before set note
-    duration_flag = 0;
+  // Reset duration_flag before set note
+  duration_flag = 0;
 
-    // Set cycle
-    int cycle_in_ms = (1 / note) * 1000;
-    buzzer->pwm_set_freq(BSP_CONFIG_ID_BUZZER, BUZZER_CHANNEL, cycle_in_ms);
+  // Set cycle
+  float cycle_in_ms = (1.0 / note) * 1000;
+  buzzer->pwm_set_freq(BSP_CONFIG_ID_BUZZER, BSP_CONFIG_BUZZER_CHANNEL, cycle_in_ms);
 
-    // Start PWM
-    buzzer->pwm_start(BSP_CONFIG_ID_BUZZER, BUZZER_CHANNEL);
+  // Start PWM
+  buzzer->pwm_start(BSP_CONFIG_ID_BUZZER, BSP_CONFIG_BUZZER_CHANNEL);
 
-    // Set timer to stop after a duration
-    buzzer->timer_set((void *)drv_buzzer_duration_cplt, DELAY_BEFORE_START, duration);
+  // Set timer to stop after a duration
+  sch_add_task(drv_buzzer_duration_callback, duration, 0);
 
-
-    return DRV_BUZZER_STATUS_SUCCESS;
+  return DRV_BUZZER_STATUS_SUCCESS;
 }
 
-uint8_t drv_buzzer_duration_cplt(drv_buzzer_t *buzzer) 
+uint8_t drv_buzzer_duration_cplt(drv_buzzer_t* buzzer)
 {
-    if (buzzer == NULL) 
-    {
-        return DRV_BUZZER_STATUS_FAIL; 
-    }
+  if (buzzer == NULL)
+  {
+    return DRV_BUZZER_STATUS_FAIL;
+  }
+  uint8_t ret_flag = duration_flag;
 
-    // Stop PWM
-    result = buzzer->pwm_stop(BSP_CONFIG_ID_BUZZER, BUZZER_CHANNEL);
+  // Stop PWM
+  if (duration_flag == 1)
+  {
+    duration_flag = 0;
+    buzzer->pwm_stop(BSP_CONFIG_ID_BUZZER, BSP_CONFIG_BUZZER_CHANNEL);
+  }
+  // Set duration_flag to indicate completion
 
-    // Set duration_flag to indicate completion
-    duration_flag = 1;
-
-    return DRV_BUZZER_STATUS_SUCCESS; 
+  return ret_flag;
 }
 
 /* Private definitions ----------------------------------------------- */
-
+void drv_buzzer_duration_callback(void)
+{
+  duration_flag = 1;
+}
 /* End of file -------------------------------------------------------- */
