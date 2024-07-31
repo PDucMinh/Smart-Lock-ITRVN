adda83492ca8255863e1072914daaad56379e196
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-07-31 13:00:17 +0700

    update: include scheduler library to driver ir sensor

--- final_project/source_code/smart_lock/device_driver/drv_ir.h
+++ final_project/source_code/smart_lock/device_driver/drv_ir.h
@@ -21,6 +21,7 @@
 #include "bsp_exti.h"
 #include "bsp_gpio.h"
 #include "bsp_state.h"
+#include "scheduler.h"
 /* Public defines ---------------------------------------------------- */
 
 /* Public enumerate/structure ----------------------------------------- */
@@ -48,7 +49,6 @@ typedef enum
 typedef struct
 {
   uint8_t debounce_flag;
-  uint32_t debounce_tick_start;              /**< define debounce time */
   uint8_t (*exti_event)(uint16_t exti_line); /**< function pointer to detect external event */
   uint8_t (*gpio_state)(uint16_t pin);       /**< function pointer to detect gpio state */
 } drv_ir_t;
