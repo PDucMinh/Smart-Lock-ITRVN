165e984b2bd4b34cfbfbb8a34da7327c30138d07
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-03 23:46:42 +0700

    update: add defintion for checking startup state API, scanning TTP229 API and refactor code in reading keypad API

--- final_project/source_code/smart_lock/device_driver/drv_keypad.c
+++ final_project/source_code/smart_lock/device_driver/drv_keypad.c
@@ -9,14 +9,13 @@
  *
  * @brief      <This file is source code file to define functions for TTP229 keypad module>
  *
- * @note 
+ * @note
  */
-
-
 
 /* Includes ----------------------------------------------------------- */
 #include "drv_keypad.h"
-
+#include "scheduler.h"
+#include "bsp_usart.h"
 /*
   --------------------
   |1 | 2 | 3 | delete|
@@ -38,139 +37,130 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-
+uint8_t btn_state[2];
+uint8_t keypad_startup_flag = 0;
 /* Private function prototypes ---------------------------------------- */
-
+void drv_keypad_startup_callback(void);
 /* Function definitions ----------------------------------------------- */
-drv_keypad_status_t drv_keypad_init(drv_keypad_t *keypad)
+drv_keypad_status_t drv_keypad_init(drv_keypad_t* keypad)
 {
   if (keypad == NULL)
   {
     return DRV_KEYPAD_STATUS_FAIL;
   }
 
-  keypad->usart_rx = bsp_usart_rx;
-  for (uint8_t i = 0; i < 16; i++) 
+  keypad->usart_rx_active = bsp_usart_receive_active;
+  keypad->usart_rx_cplt = bsp_usart_receive_cplt;
+  keypad->timer_set = sch_add_task;
+  for (uint8_t i = 0; i < 2; i++)
   {
-    keypad->pre_btn[i] = 1; // active-low, 1 --> not pressed
+    keypad->pre_btn[i] = 0xFF; // active-low, 1 --> not pressed
   }
-
+  keypad->timer_set(drv_keypad_startup_callback, 5000, 0);
   return DRV_KEYPAD_STATUS_SUCCESS;
 }
 
-drv_keypad_button_t drv_keypad_read(drv_keypad_t *keypad)
+drv_keypad_status_t drv_keypad_startup_state(drv_keypad_t* keypad)
+{
+  return keypad_startup_flag;
+}
+
+drv_keypad_button_t drv_keypad_scan(drv_keypad_t* keypad)
+{
+  if (keypad == NULL)
+  {
+    return DRV_KEYPAD_STATUS_FAIL;
+  }
+  keypad->usart_rx_active(BSP_CONFIG_ID_KEYPAD, btn_state, 2);
+  return DRV_KEYPAD_STATUS_SUCCESS;
+}
+
+drv_keypad_button_t drv_keypad_read(drv_keypad_t* keypad)
 {
   if (keypad == NULL)
   {
     return DRV_KEYPAD_ERROR;
   }
 
-  uint8_t rx_data[2];
-  uint16_t key_value;
-  uint8_t buttons_pressed[16] = {0};
-  uint8_t count_buttons_pressed = 0;
-  uint8_t btn_pressed_position = 20;
+  uint16_t keypad_value;
 
   // Receive data from usart
-  if (keypad->usart_rx(BSP_CONFIG_KEYPAD, rx_data, 2) == 1) //  1 --> receive successed
+  if (keypad->usart_rx_cplt(BSP_CONFIG_ID_KEYPAD))
   {
-    key_value = (rx_data[0] << 8) | rx_data[1];
-
-    for (uint8_t i = 0; i < 16; i++) 
-    {
-      if (key_value & (1 << i)) 
-      {
-        buttons_pressed[i] = 1;
-
-        // Count the number of button pressed and returned the location
-        count_buttons_pressed = count_buttons_pressed + 1;
-        btn_pressed_position = i + 1;
-      }
-    }
-  } 
-  else                                                      //  0 --> receive failed
-  {
-    return DRV_KEYPAD_ERROR;
-  }
-
-
-  // Only 1 button is allowed per time point
-  if ((count_buttons_pressed = 0) || (count_buttons_pressed > 1))
-  {
-    // Reset state of all buttons when keypad were released
-    for (uint8_t i = 0; i < 16; i++) 
-    {
-      keypad->pre_btn[i] = 1; // active-low, 1 --> not pressed
-    }
-    return DRV_KEYPAD_ERROR;
-  }
-
-
-  // Check if button was received before
-  // If button is held, it will receive 1 time
-  if (keypad->pre_btn[btn_pressed_position - 1] == 0)
-  {
-    return DRV_KEYPAD_ERROR;
+    keypad_value = ((int)btn_state[1] << 8) | btn_state[0];
   }
   else
   {
-    keypad->pre_btn[btn_pressed_position - 1] = 0; // active-low, 0 --> pressed
-    switch (btn_pressed_position)
+    return DRV_KEYPAD_NONE;
+  }
+
+  uint8_t keypad_position;
+  for (int i = 0; i < 16; i++)
+  {
+    if (!(keypad_value & (1 < i)))
     {
-    case 1:
-      return DRV_KEYPAD_BUTTON_1;
-      break;
-    case 2:
-      return DRV_KEYPAD_BUTTON_2;
-      break;
-    case 3:
-      return DRV_KEYPAD_BUTTON_3;
-      break;
-    case 4:
-      return DRV_KEYPAD_BUTTON_DELETE;
-      break;
-    case 5:
-      return DRV_KEYPAD_BUTTON_4;
-      break;
-    case 6:
-      return DRV_KEYPAD_BUTTON_5;
-      break;
-    case 7:
-      return DRV_KEYPAD_BUTTON_6;
-      break;
-    case 8:
-      return DRV_KEYPAD_BUTTON_UP;
-      break;
-    case 9:
-      return DRV_KEYPAD_BUTTON_7;
-      break;
-    case 10:
-      return DRV_KEYPAD_BUTTON_8;
-      break;
-    case 11:
-      return DRV_KEYPAD_BUTTON_9;
-      break;
-    case 12:
-      return DRV_KEYPAD_BUTTON_DOWN;
-      break;
-    case 13:
-      return DRV_KEYPAD_BUTTON_STAR;
-      break;
-    case 14:
-      return DRV_KEYPAD_BUTTON_0;
-      break;
-    case 15:
-      return DRV_KEYPAD_BUTTON_SHARP;
-      break;
-    case 16:
-      return DRV_KEYPAD_BUTTON_ENTER;
-      break;
-    default:
-      return DRV_KEYPAD_ERROR;
-      break;
+      if (!(keypad->pre_btn & (1 < i)))
+      {
+        return DRV_KEYPAD_NONE;
+      }
+      else
+      {
+        keypad->pre_btn &= ~(1 < i);
+        return DRV_KEYPAD_NONE;
+      }
     }
+    else
+    {
+      if (!(keypad->pre_btn & (1 < i)))
+      {
+        keypad_position = i;
+        break;
+      }
+      else
+      {
+        return DRV_KEYPAD_NONE;
+      }
+    }
+  }
+  switch (keypad_position)
+  {
+  case 1:
+    return DRV_KEYPAD_BUTTON_1;
+  case 2:
+    return DRV_KEYPAD_BUTTON_2;
+  case 3:
+    return DRV_KEYPAD_BUTTON_3;
+  case 4:
+    return DRV_KEYPAD_BUTTON_DELETE;
+  case 5:
+    return DRV_KEYPAD_BUTTON_4;
+  case 6:
+    return DRV_KEYPAD_BUTTON_5;
+  case 7:
+    return DRV_KEYPAD_BUTTON_6;
+  case 8:
+    return DRV_KEYPAD_BUTTON_UP;
+  case 9:
+    return DRV_KEYPAD_BUTTON_7;
+  case 10:
+    return DRV_KEYPAD_BUTTON_8;
+  case 11:
+    return DRV_KEYPAD_BUTTON_9;
+  case 12:
+    return DRV_KEYPAD_BUTTON_DOWN;
+  case 13:
+    return DRV_KEYPAD_BUTTON_STAR;
+  case 14:
+    return DRV_KEYPAD_BUTTON_0;
+  case 15:
+    return DRV_KEYPAD_BUTTON_SHARP;
+  case 16:
+    return DRV_KEYPAD_BUTTON_ENTER;
   }
 }
 /* Private definitions ----------------------------------------------- */
-
+void drv_keypad_startup_callback(void)
+{
+  keypad_startup_flag = 1;
+}
 /* End of file -------------------------------------------------------- */
