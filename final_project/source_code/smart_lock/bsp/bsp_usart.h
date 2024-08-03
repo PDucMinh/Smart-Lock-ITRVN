b55aa37d01fdbce49e5a3c57d5ca40901dc7b9ea
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-03 17:42:58 +0700

    feat: add initial API prototype for bsp usart

---
+++ final_project/source_code/smart_lock/bsp/bsp_usart.h
@@ -0,0 +1,64 @@
+/**
+ * @file       bsp_usart.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license
+ * @version    major.minor.patch
+ * @date       2024-08-03
+ * @author     Hung Nguyen
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __BSP_USART_H
+#define __BSP_USART_H
+
+/* Includes ----------------------------------------------------------- */
+#include "stm32f4xx_hal.h"
+#include "bsp_state.h"
+#include "bsp_mcu.h"
+#include "bsp_config.h"
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+bsp_state_t bsp_usart_init(bsp_mcu_t* mcu);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+bsp_state_t bsp_usart_receive(bsp_config_id_t id, uint8_t* pdata, uint16_t size);
+#endif // __BSP_USART_H
+
+/* End of file -------------------------------------------------------- */
