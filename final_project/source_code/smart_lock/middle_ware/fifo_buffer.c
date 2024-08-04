f4794a18fcde3cf8bf3359fe82a1c0fa15ee747a
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-04 13:53:18 +0700

    update: update definition for fifo API

--- final_project/source_code/smart_lock/middle_ware/fifo_buffer.c
+++ final_project/source_code/smart_lock/middle_ware/fifo_buffer.c
@@ -1,11 +1,10 @@
 /**
  * @file       fifo_buffer.c
  * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
- * @license    This project is released under the Fiot License.
- * @version    major.minor.patch
- * @date       yyyy-mm-dd
- * @author     <first_name_1> <last_name_1>
- * @author     <first_name_2> <last_name_2>
+ * @license    
+ * @version    1.0.0
+ * @date       2024-08-04
+ * @author     Minh Pham
  *
  * @brief      <A brief description of the content of the file>
  *
@@ -28,134 +27,96 @@
 /* Public variables --------------------------------------------------- */
 
 /* Private variables -------------------------------------------------- */
-
+static uint8_t fifo_pool[FIFO_MAX_NUM][FIFO_MAX_SIZE];
+static uint8_t fifo_index = 0;
 /* Private function prototypes ---------------------------------------- */
-/**
- * @brief  <function description>
- *
- * @param[in]     <param_name>  <param_despcription>
- * @param[out]    <param_name>  <param_despcription>
- * @param[inout]  <param_name>  <param_despcription>
- *
- * @attention  <API attention note>
- *
- * @return
- *  - 0: Success
- *  - 1: Error
- */
-fifo_return_t fifo_set_head(fifo_buffer_info_t *fifo_buffer, uint8_t head);
-
-/**
- * @brief  <function description>
- *
- * @param[in]     <param_name>  <param_despcription>
- * @param[out]    <param_name>  <param_despcription>
- * @param[inout]  <param_name>  <param_despcription>
- *
- * @attention  <API attention note>
- *
- * @return
- *  - 0: Success
- *  - 1: Error
- */
-fifo_return_t fifo_set_tail(fifo_buffer_info_t *fifo_buffer, uint8_t tail);
 
 /* Function definitions ----------------------------------------------- */
-void fifo_init(fifo_buffer_info_t *fifo_buffer, uint32_t fifo_size)
+void fifo_init(fifo_buffer_info_t* fifo_buffer, uint16_t fifo_size, uint32_t info_size)
 {
-  if ((fifo_size > FIFO_MAX_SIZE) || (fifo_size == 0))
+  if (((fifo_size * info_size) > FIFO_MAX_SIZE) || (fifo_size == 0) || (info_size == 0))
+  {
+    return;
+  }
+  if (fifo_buffer == NULL)
+  {
+    return;
+  }
+  if (fifo_index == FIFO_MAX_NUM)
   {
     return;
   }
   fifo_buffer->fifo_size = fifo_size;
-  fifo_set_head(fifo_buffer, 0);
-  fifo_set_tail(fifo_buffer, 0);
+  fifo_buffer->info_size = info_size;
+  fifo_buffer->pbuf = *fifo_pool + fifo_index;
+  fifo_index = fifo_index + 1;
+  fifo_buffer->tail = 0;
 }
 
 void fifo_empty(fifo_buffer_info_t *fifo_buffer)
 {
-  if (fifo_buffer->head == fifo_buffer->tail)
+  if (fifo_buffer->tail == 0)
   {
     return;
   }
-  fifo_set_head(fifo_buffer, 0);
-  fifo_set_tail(fifo_buffer, 0);
-  // something
+  fifo_buffer->tail = 0;
 }
 
-uint8_t fifo_push(fifo_buffer_info_t *fifo_buffer, fifo_buffer_info_t *source_data)
+fifo_return_t fifo_push(fifo_buffer_info_t* fifo_buffer, void* source_data)
 {
-  if (fifo_buffer->tail >= fifo_buffer->fifo_size)
+  if (fifo_buffer == NULL)
   {
     return FIFO_FAIL;
   }
-  else 
+  else if (source_data == NULL)
   {
-    fifo_buffer_info_t *temp_buffer = fifo_buffer;
-    // temp_buffer = temp_buffer + fifo_buffer->tail;
-    // temp_buffer = source_data;
-    *(temp_buffer + fifo_buffer->tail) = *source_data;
-    fifo_buffer->tail = fifo_buffer->tail + 1;
-    fifo_set_tail(fifo_buffer, fifo_buffer->tail);
+    return FIFO_FAIL;
   }
-  // something
+  else if (fifo_buffer->tail > (fifo_buffer->fifo_size))
+  {
+    return FIFO_FAIL;
+  }
+  else
+  {
+    uint8_t* temp_pbuf = fifo_buffer->pbuf + (fifo_buffer->tail)*(fifo_buffer->info_size);
+    memcpy((void *)temp_pbuf, source_data, fifo_buffer->info_size);
+    fifo_buffer->tail = fifo_buffer->tail + 1;
+  }
   return FIFO_OK;
 }
 
-uint8_t fifo_pop(fifo_buffer_info_t *fifo_buffer, fifo_buffer_info_t *dest_data)
+fifo_return_t fifo_pop(fifo_buffer_info_t* fifo_buffer, void* dest_data)
 {
-  if (fifo_buffer->head == fifo_buffer->tail)
+  if (fifo_buffer == NULL)
   {
     return FIFO_FAIL;
   }
-  else 
+  else if (dest_data == NULL)
   {
-    *dest_data = *(fifo_buffer + fifo_buffer->head);
-    fifo_buffer_info_t *temp_data = fifo_buffer + fifo_buffer->head;
-    for (uint16_t i = 0; i < fifo_buffer->fifo_size - 1; i++)
+    return FIFO_FAIL;
+  }
+  if (fifo_buffer->tail == 0)
+  {
+    return FIFO_FAIL;
+  }
+  else
+  {
+    uint8_t* temp_pbuf = fifo_buffer->pbuf;
+    memcpy(dest_data,(void *)temp_pbuf, fifo_buffer->info_size);
+    for (uint16_t i = 0; i < fifo_buffer->fifo_size; i++)
     {
-      *temp_data = *(temp_data + 1);
-      temp_data++;
+      memmove((void *)temp_pbuf, (void *)(temp_pbuf + fifo_buffer->info_size), fifo_buffer->info_size);
+      temp_pbuf = temp_pbuf + fifo_buffer->info_size;
     }
     fifo_buffer->tail = fifo_buffer->tail - 1;
-    fifo_set_tail(fifo_buffer, fifo_buffer->tail);
   }
-  // something
   return FIFO_OK;
 }
 
 uint8_t fifo_size(fifo_buffer_info_t *fifo_buffer)
 {
-  return fifo_buffer->tail - fifo_buffer->head;
+  return fifo_buffer->tail + 1;
 }
 /* Private definitions ----------------------------------------------- */
-fifo_return_t fifo_set_head(fifo_buffer_info_t *fifo_buffer, uint8_t head)
-{
-  if (head != 0)
-  {
-    return FIFO_FAIL;
-  }
-  for (uint16_t i = 0; i < fifo_buffer->fifo_size; i++)
-  {
-    fifo_buffer->head = head;
-  }
-  return FIFO_OK;
-}
 
-fifo_return_t fifo_set_tail(fifo_buffer_info_t *fifo_buffer, uint8_t tail)
-{
-  if (tail < 0)
-  {
-    return FIFO_FAIL;
-  }
-  if (tail > fifo_buffer->fifo_size)
-  {
-    tail = 0;
-  }
-  for (uint16_t i = 0; i < fifo_buffer->fifo_size; i++)
-  {
-    fifo_buffer->tail = tail;
-  }
-  return FIFO_OK;
-}
 /* End of file -------------------------------------------------------- */
