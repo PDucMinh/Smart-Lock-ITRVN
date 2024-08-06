c9f4bdbd3a81d48e8f98c650b17f78e436bc0d8c
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-08-06 20:26:20 +0700

    feat(sys_protocol): add read_string()

--- final_project/source_code/smart_lock/system/sys_protocol.c
+++ final_project/source_code/smart_lock/system/sys_protocol.c
@@ -41,6 +41,37 @@ static void private_function(void);
 /* Function definitions ----------------------------------------------- */
 void read_data_frame_from_string(data_frame_t *dest_data_frame, char *src_data_string)
 {
+  data_frame_t data_f;
+
+  // First sscanf call to parse header, sequence, command, and length
+  int result1 = sscanf(src_data_string,
+                       "%2hhx%2hhx%2hhx%2hhx%*x",
+                       &data_f.header,
+                       &data_f.sequence,
+                       &data_f.command,
+                       &data_f.length);
+
+  // Check if the first parsing was successful
+  // Second sscanf call to parse data and CRC based on the length
+  uint8_t temp;
+  for (int i = 0; i < data_f.length; i++)
+  {
+    sscanf(src_data_string + 8 + i * 2, "%2hhx%*x", &temp);
+    data_f.data[i] = temp;
+  }
+  for (int i = 0; *(src_data_string + 8 + data_f.length * 2 + i * 2) != '\0'; i++)
+  {
+    sscanf(src_data_string + 8 + data_f.length * 2 + i * 2, "%2hhx%*x", &temp);
+    data_f.crc[i] = temp;
+  }
+  dest_data_frame->header = data_f.header;
+  dest_data_frame->sequence = data_f.sequence;
+  dest_data_frame->command = data_f.command;
+  dest_data_frame->length = data_f.length;
+  for (int i = 0; i < MAX_DATA_LENGTH; i++)
+    dest_data_frame->data[i] = data_f.data[i];
+  for (int i = 0; i < MAX_CRC_LENGTH; i++)
+    dest_data_frame->crc[i] = data_f.crc[i];
 }
 
 char write_data_frame_to_string(data_frame_t *src_data_frame)
