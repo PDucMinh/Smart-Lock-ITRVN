89b47d39420399fa518c1859a8200541f6daf731
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-08-06 10:39:41 +0700

    chore: prototype for data encryption

--- final_project/source_code/smart_lock/middle_ware/encryption.c
+++ final_project/source_code/smart_lock/middle_ware/encryption.c
@@ -17,58 +17,17 @@
  */
 
 /* Includes ----------------------------------------------------------- */
-#include "code_template.h"
+#include "encryption.h"
 
 /* Private defines ---------------------------------------------------- */
-#define PRIVATE_DEFINE_1  (0) /*!< Description of PRIVATE_DEFINE_1 */
-#define PRIVATE_DEFINE_2  (0) /*!< Description of PRIVATE_DEFINE_2 */
 
 /* Private enumerate/structure ---------------------------------------- */
-/**
- * @brief <enum descriptiton>
- */
-typedef enum 
-{
-  PRIVATE_ENUM_1, /**< Description of PRIVATE_ENUM_1 */
-  PRIVATE_ENUM_2, /**< Description of PRIVATE_ENUM_2 */
-  PRIVATE_ENUM_3  /**< Description of PRIVATE_ENUM_3 */
-}
-private_enum_type_t;
-
-/**
- * @brief <structure descriptiton>
- */
-typedef struct 
-{
-  uint32_t member_1, /**< Description of member_1 */
-  uint32_t member_2, /**< Description of member_2 */
-  uint32_t member_3  /**< Description of member_3 */
-}
-private_struct_type_t;
 
 /* Private macros ----------------------------------------------------- */
-/**
- * @brief  <macro description>
- *
- * @param[in]     <param_name>  <param_despcription>
- * @param[out]    <param_name>  <param_despcription>
- * @param[inout]  <param_name>  <param_despcription>
- *
- * @attention  <API attention note>
- *
- * @return  
- *  - 0: Success
- *  - 1: Error
- */
-#define PRIVATE_MACRO(a)  do_something_with(a)
 
 /* Public variables --------------------------------------------------- */
-int g_var_1;
-int g_var_2;
 
 /* Private variables -------------------------------------------------- */
-static int var_1; /**< Description of private variable var_1 */
-static int var_2; /**< Description of private variable var_2 */
 
 /* Private function prototypes ---------------------------------------- */
 /**
@@ -86,11 +45,25 @@ static int var_2; /**< Description of private variable var_2 */
  */
 static void private_function(void);
 /* Function definitions ----------------------------------------------- */
-void public_function(void)
+char *encode_string(const char *string)
 {
+
+}
+
+char *encode_number(const int *number)
+{
+
+}
+
+char *encode_log_info(log_info_t *log_info)
+{
+
+}
+
+int str_len(char *str)
+{
+
 }
 /* Private definitions ----------------------------------------------- */
-static void private_function(void)
-{
-}
+
 /* End of file -------------------------------------------------------- */
---
+++ final_project/source_code/smart_lock/middle_ware/encryption.h
@@ -0,0 +1,110 @@
+/**
+ * @file       <file_name>.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    major.minor.patch
+ * @date       yyyy-mm-dd
+ * @author     <first_name_1> <last_name_1>
+ * @author     <first_name_2> <last_name_2>
+ *             
+ * @brief      <A brief description of the content of the file>
+ *             
+ * @note          
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __ENCRYPTION_H
+#define __ENCRYPTION_H
+
+/* Includes ----------------------------------------------------------- */
+#include "string.h"
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+/**
+ * @brief define user information structure
+ */
+typedef struct
+{
+    char *key;
+    int rfid;
+    char *user_name;
+    char *encoded_password;
+    char *type;
+} user_info_t;
+
+/**
+ * @brief define log structure before encoded to send to UI
+ */
+typedef struct
+{
+    int time_stamp[6];
+    char *user_name;
+    char *access;
+    char *user_hierarchy;
+} log_info_t;
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  bencode encoding string
+ *
+ * @param[in]     string  array of characters
+ * @param[out]    encoded_string array of encoded string
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Error
+ *  - Others: Encoded string
+ */
+char *encode_string(const char *string);
+
+/**
+ * @brief  bencode encoding integer number
+ *
+ * @param[in]     number array of integer
+ * @param[out]    encoded_number array of string contain encoded number 
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Error
+ *  - Others: Encoded number
+ */
+char *encode_number(const int *number);
+
+/**
+ * @brief  bencode encoding log information
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Error
+ *  - Others: Encoded log information
+ */
+char *encode_log_info(log_info_t *log_info);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     str array of character
+ * @param[out]    str_len length of string
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ */
+int str_len(char *str);
+
+
+#endif // __CODE_TEMPLATE_H
+
+/* End of file -------------------------------------------------------- */
