02cb3f40705b4cf086c66591627342c6bf113c59
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-07 16:28:35 +0700

    feat: add definition for bsp i2c API

---
+++ final_project/source_code/smart_lock/bsp/bsp_i2c.c
@@ -0,0 +1,148 @@
+/**
+ * @file       bsp_i2c.c
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license
+ * @version    1.0.0
+ * @date       2024-08-07
+ * @author     Hung Nguyen
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example
+ *
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "bsp_i2c.h"
+
+/* Private defines ---------------------------------------------------- */
+
+/* Private enumerate/structure ---------------------------------------- */
+
+/* Private macros ----------------------------------------------------- */
+
+/* Public variables --------------------------------------------------- */
+
+/* Private variables -------------------------------------------------- */
+static int var_1; /**< Description of private variable var_1 */
+static int var_2; /**< Description of private variable var_2 */
+static I2C_HandleTypeDef hi2c1;
+static uint8_t tx_cplt = 0;
+static uint8_t rx_cplt = 0;
+/* Private function prototypes ---------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+static void bsp_i2c_transmit_handler(I2C_HandleTypeDef* hi2c);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+static void bsp_i2c_receive_handler(I2C_HandleTypeDef* hi2c);
+/* Function definitions ----------------------------------------------- */
+bsp_state_t bsp_i2c_init(bsp_mcu_t* mcu)
+{
+  BSP_CHECK_NULL(mcu, BSP_STATE_FAIL);
+  hi2c1 = mcu->hi2c1;
+  hi2c1.MemTxCpltCallback = bsp_i2c_transmit_handler;
+  hi2c1.MemRxCpltCallback = bsp_i2c_receive_handler;
+  return BSP_STATE_PASS;
+}
+
+bsp_state_t bsp_i2c_transmit_start(bsp_config_id_t id,
+                                   uint16_t slave_addr,
+                                   uint16_t reg_addr,
+                                   uint8_t* pdata,
+                                   uint16_t data_size);
+{
+  BSP_CHECK_NULL(pdata, BSP_STATE_FAIL);
+  BSP_CHECK_NULL(data_size, BSP_STATE_FAIL);
+  BSP_CHECK_RANGE(id, BSP_CONFIG_ID_MAX, BSP_STATE_FAIL);
+  if (id == BSP_CONFIG_ID_RTC)
+  {
+    if (HAL_I2C_Mem_Write_IT(&hi2c1, slave_addr, reg_addr, I2C_MEMADD_SIZE_8BIT, pdata, data_size) != HAL_OK)
+    {
+      return BSP_STATE_FAIL;
+    }
+  }
+  return BSP_STATE_PASS;
+}
+
+bsp_state_t bsp_i2c_receive_start(bsp_config_id_t id,
+                                  uint16_t slave_addr,
+                                  uint16_t reg_addr,
+                                  uint8_t* pdata,
+                                  uint16_t data_size);
+{
+  BSP_CHECK_NULL(pdata, BSP_STATE_FAIL);
+  BSP_CHECK_NULL(data_size, BSP_STATE_FAIL);
+  BSP_CHECK_RANGE(id, BSP_CONFIG_ID_MAX, BSP_STATE_FAIL);
+  if (id == BSP_CONFIG_ID_RTC)
+  {
+    if (HAL_I2C_Mem_Read_IT(&hi2c1, slave_addr, reg_addr, I2C_MEMADD_SIZE_8BIT, pdata, data_size) != HAL_OK)
+    {
+      return BSP_STATE_FAIL;
+    }
+  }
+  return BSP_STATE_PASS;
+}
+
+uint8_t bsp_i2c_transmit_cplt(bsp_config_id_t id)
+{
+  if (id == BSP_CONFIG_ID_RTC)
+  {
+    uint8_t ret_flag = 0;
+    ret_flag = tx_cplt;
+    if (tx_cplt == 1)
+    {
+      tx_cplt = 0;
+    }
+    return ret_flag;
+  }
+}
+
+uint8_t bsp_i2c_receive_cplt(bsp_config_id_t id)
+{
+  if (id == BSP_CONFIG_ID_RTC)
+  {
+    uint8_t ret_flag = 0;
+    ret_flag = rx_cplt;
+    if (rx_cplt == 1)
+    {
+      rx_cplt = 0;
+    }
+    return ret_flag;
+  }
+}
+/* Private definitions ----------------------------------------------- */
+static void bsp_i2c_transmit_handler(I2C_HandleTypeDef* hi2c)
+{
+    tx_cplt = 1;
+}
+
+static void bsp_i2c_receive_handler(I2C_HandleTypeDef* hi2c)
+{
+    rx_cplt = 1;
+}
+/* End of file -------------------------------------------------------- */
