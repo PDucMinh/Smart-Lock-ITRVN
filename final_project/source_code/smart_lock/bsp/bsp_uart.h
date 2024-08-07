a398dd1fa64a719e5ffc1475eabc9794f7a19d02
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-07 09:26:24 +0700

    feat: add API prototype  bsp uart module

---
+++ final_project/source_code/smart_lock/bsp/bsp_uart.h
@@ -0,0 +1,111 @@
+/**
+ * @file       bsp_uart.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license
+ * @version    1.0.0
+ * @date       2024-08-08
+ * @author     Hung Nguyen
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __BSP_UART_H
+#define __BSP_UART_H
+
+/* Includes ----------------------------------------------------------- */
+#include "stm32f4xx_hal.h"
+#include "bsp_config.h"
+#include "bsp_state.h"
+#include "bsp_mcu.h"
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+bsp_state_t bsp_uart_init(bsp_mcu_t* mcu);
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+bsp_state_t bsp_uart_transmit_start(bsp_config_id_t id, uint8_t* pdata, uint16_t size);
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint8_t bsp_uart_transmit_cplt(bsp_config_id_t id);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+bsp_state_t bsp_uart_receive_start(bsp_config_id_t id, uint8_t* pdata, uint16_t size);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint8_t bsp_uart_receive_cplt(bsp_config_id_t id);
+
+#endif // __BSP_UART_H
+
+/* End of file -------------------------------------------------------- */
