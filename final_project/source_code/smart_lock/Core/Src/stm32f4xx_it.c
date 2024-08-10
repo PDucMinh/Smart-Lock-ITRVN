176ac87d935fae19963b25096ff745ea3df7cd9f
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-11 01:01:35 +0700

    feat: add interrupt handler for i2c2

--- final_project/source_code/smart_lock/Core/Src/stm32f4xx_it.c
+++ final_project/source_code/smart_lock/Core/Src/stm32f4xx_it.c
@@ -58,6 +58,9 @@
 
 /* USER CODE BEGIN EV */
 extern UART_HandleTypeDef huart6;
+extern I2C_HandleTypeDef hi2c1;
+extern I2C_HandleTypeDef hi2c2;
+
 /* USER CODE END EV */
 
 /******************************************************************************/
@@ -238,6 +241,17 @@ void I2C1_EV_IRQHandler(void)
 
   /* USER CODE END I2C1_EV_IRQn 0 */
   HAL_I2C_EV_IRQHandler(&hi2c1);
+  /* USER CODE BEGIN I2C1_EV_IRQn 1 */
+
+  /* USER CODE END I2C1_EV_IRQn 1 */
+}
+
+void I2C2_EV_IRQHandler(void)
+{
+  /* USER CODE BEGIN I2C1_EV_IRQn 0 */
+
+  /* USER CODE END I2C1_EV_IRQn 0 */
+  HAL_I2C_EV_IRQHandler(&hi2c2);
   /* USER CODE BEGIN I2C1_EV_IRQn 1 */
 
   /* USER CODE END I2C1_EV_IRQn 1 */
