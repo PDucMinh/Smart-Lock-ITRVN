29045b2b73a9051a1786ae96dfc376c0e74c23b5
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-11 11:28:47 +0700

    feat: add initial bsp flash API

---
+++ final_project/source_code/smart_lock/bsp/bsp_flash.h
@@ -0,0 +1,79 @@
+/**
+ * @file       bsp_flash.h
+ * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
+ * @license    
+ * @version    1.0.0
+ * @date       2024-08-11
+ * @author     Hung Nguyen
+ *             
+ * @brief      <A brief description of the content of the file>
+ *             
+ * @note          
+ * @example    
+ * 
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __BSP_FLASH_H
+#define __BSP_FLASH_H
+
+/* Includes ----------------------------------------------------------- */
+#include "bsp_state.h"
+/* Public defines ----------------------------------------------------- */
+#define BSP_FLASH_START_ADDR_SECT6  (0x08040000) /*!< Description of PUBLIC_DEFINE_1 */
+#define BSP_FLASH_START_ADDR_SECT7  (0x08060000) /*!< Description of PUBLIC_DEFINE_2 */
+
+/* Public enumerate/structure ----------------------------------------- */
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+ bsp_state_t bsp_flash_write_register(uint32_t addr, uint64_t data);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint8_t bsp_flash_write_cplt(void);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint32_t bsp_flash_read(uint32_t addr);
+#endif // __BSP_FLASH_H
+
+/* End of file -------------------------------------------------------- */
