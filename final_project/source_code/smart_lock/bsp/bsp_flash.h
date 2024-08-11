b81c1a394bf817eacfd4d0c9ff0eee6492d24d3b
hungnguyennhat-itr-intern <nguyennhathung2502@gmail.com>
2024-08-11 14:22:26 +0700

    feat: add erase flash sector API

--- final_project/source_code/smart_lock/bsp/bsp_flash.h
+++ final_project/source_code/smart_lock/bsp/bsp_flash.h
@@ -1,16 +1,16 @@
 /**
  * @file       bsp_flash.h
  * @copyright  Copyright (C) 2019 ITRVN. All rights reserved.
- * @license    
+ * @license
  * @version    1.0.0
  * @date       2024-08-11
  * @author     Hung Nguyen
- *             
+ *
  * @brief      <A brief description of the content of the file>
- *             
- * @note          
- * @example    
- * 
+ *
+ * @note
+ * @example
+ *
  */
 
 /* Define to prevent recursive inclusion ------------------------------ */
@@ -20,9 +20,9 @@
 /* Includes ----------------------------------------------------------- */
 #include "bsp_state.h"
 /* Public defines ----------------------------------------------------- */
-#define BSP_FLASH_START_ADDR_SECT6  (0x08040000) /*!< Description of PUBLIC_DEFINE_1 */
-#define BSP_FLASH_START_ADDR_SECT7  (0x08060000) /*!< Description of PUBLIC_DEFINE_2 */
-
+#define BSP_FLASH_START_ADDR_SECT6 (0x08040000) /*!< Description of PUBLIC_DEFINE_1 */
+#define BSP_FLASH_START_ADDR_SECT7 (0x08060000) /*!< Description of PUBLIC_DEFINE_2 */
+#define BSP_FLASH_SECT_SIZE        (0x00020000)
 /* Public enumerate/structure ----------------------------------------- */
 
 /* Public macros ------------------------------------------------------ */
@@ -39,11 +39,11 @@
  *
  * @attention  <API attention note>
  *
- * @return  
+ * @return
  *  - 0: Success
  *  - 1: Error
  */
- bsp_state_t bsp_flash_write_register(uint32_t addr, uint64_t data);
+bsp_state_t bsp_flash_write_register(uint32_t addr, uint64_t data);
 
 /**
  * @brief  <function description>
@@ -54,7 +54,7 @@
  *
  * @attention  <API attention note>
  *
- * @return  
+ * @return
  *  - 0: Success
  *  - 1: Error
  */
@@ -69,7 +69,37 @@ uint8_t bsp_flash_write_cplt(void);
  *
  * @attention  <API attention note>
  *
- * @return  
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+bsp_state_t bsp_flash_erase_register(uint32_t addr);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+uint8_t bsp_flash_erase_cplt(void);
+
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
  *  - 0: Success
  *  - 1: Error
  */
